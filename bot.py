d3eefd599e42ce01eb3142b6bac8805f9028c667
faiazshawn-boop <faiazshawn@gmail.com>
2025-11-02 22:41:55 +0600

    bot.py

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ import re
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from flask import Flask, request, jsonify, abort # <-- ওয়েব সার্ভার
+from flask_cors import CORS # <-- এই নতুন লাইনটি যোগ করুন
 import logging
 
 # --- লগিং সেটআপ ---
